0e4755b4f5007dc1ef138144891d1b465e379c60
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2016-01-05 11:19:57 -0800

    create Header component

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ var config =
 	{
 		html: './src/*.html',
 		js: './src/**/*.js',
+		images: './src/images/*',
 		css:
 		[
 			'node_modules/bootstrap/dist/css/bootstrap.min.css',
@@ -76,6 +77,15 @@ gulp.task('css', function()
 		.pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
+gulp.task('images', function()
+{
+	gulp.src(config.paths.images)
+		.pipe(gulp.dest(config.paths.dist + '/images'))
+		.pipe(connect.reload());
+
+	gulp.src('./src/favicon.ico')
+		.pipe(gulp.dest(config.paths.dist));
+});
 
 gulp.task('lint', function()
 {
@@ -91,4 +101,4 @@ gulp.task('watch', function()
 });
 
 
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'images', 'lint', 'open', 'watch']);
