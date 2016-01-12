efa02abd91b90b5d67bc879013907f9ca46d3868
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2016-01-12 09:20:02 -0800

    build controller view

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var React = require('react');
+var AuthorForm = require('./authorForm');
 
 var ManageAuthorPage = React.createClass(
 {
 	render: function()
 	{
 		return (
-			<h1>Manage Author</h1>
+			<div>
+				<h1>Manage Author</h1>
+				<AuthorForm />
+			</div>
 		);
 	}
 });
